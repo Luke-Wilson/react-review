e828d048617a77a5314f06aac4a74efc27a733b9
Luke Wilson <wilson.palooza@gmail.com>
2017-01-29 19:38:10 -0800

    Add webpack middleware, webpack, and webpack config to server.js Add compiler to server.js --> webpack(webpackConfig)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 
+//webpack middleware
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
 
 var app = express();
+
+//add compiler
+var compiler = webpack(webpackConfig);
+
+
 
 
 var port = 3003;
